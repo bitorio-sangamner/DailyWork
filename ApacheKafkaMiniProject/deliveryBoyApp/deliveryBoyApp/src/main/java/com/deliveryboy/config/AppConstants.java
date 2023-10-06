1dd72c6cf019a587c73942430a31bd9f5ccd7086
Vaishnavi <vaishnavishinde93076@gmail.com>
2023-10-06 18:07:10 +0530

    6/10/2023  mini project using Apache Kafka
    Mini project using Apache Kafka

---
+++ ApacheKafkaMiniProject/deliveryBoyApp/deliveryBoyApp/src/main/java/com/deliveryboy/config/AppConstants.java
@@ -0,0 +1,9 @@
+package com.deliveryboy.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConstants {
+
+    public static final String LOCATION_TOPIC_NAME="location-update-topic";
+}
---
+++ ApacheKafkaMiniProject/enduser/enduser/src/main/java/com/enduser/AppConstants.java
@@ -0,0 +1,10 @@
+package com.enduser;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppConstants {
+    public static final String LOCATION_UPDATE_TOPIC="location-update-topic";
+
+    public static final String GROUP_ID="group-1";
+}
