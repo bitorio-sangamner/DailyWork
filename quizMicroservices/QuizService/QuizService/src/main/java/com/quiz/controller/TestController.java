10901c2c4f687e6a61a228468bc7d21fd8786868
Vaishnavi <vaishnavishinde93076@gmail.com>
2024-02-06 17:56:30 +0530

    Today's Microservices Code
    Today's Microservices Code

---
+++ quizMicroservices/QuizService/QuizService/src/main/java/com/quiz/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.quiz.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/quiz-test")
+public class TestController {
+
+    @GetMapping
+    public String quizTest()
+    {
+        return "This is quiz test controller";
+    }
+}
